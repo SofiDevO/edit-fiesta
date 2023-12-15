90406e53cbf66050f9bbeca3c98eb810ecc8fc92
SofiDevO <angiemeow89@gmail.com>
2023-12-15 15:37:21 -0600

    fixing responsive

--- assets/data/insumos.js
+++ assets/data/insumos.js
@@ -1,74 +1,74 @@
 export const insumosProductos = [
   {
     nombre: "Jumex Toronja",
-    img: "../assets/img/insumos/13.png",
+    img: "./assets/img/insumos/13.png",
   },
   {
     nombre: "Agua Mineral 1.5Lt",
-    img: "../assets/img/insumos/12.jpg",
+    img: "./assets/img/insumos/12.jpg",
   },
   {
     nombre: "Agua Quina (Six)",
-    img: "../assets/img/insumos/14.jfif",
+    img: "./assets/img/insumos/14.jfif",
   },
   {
     nombre: "Sprite 3Lt",
-    img: "../assets/img//insumos/sprite.jpg",
+    img: "./assets/img//insumos/sprite.jpg",
   },
   {
     nombre: "Pinchos",
-    img: "../assets/img//insumos/21.jpg",
+    img: "./assets/img//insumos/21.jpg",
   },
   {
     nombre: "Sal de mar",
-    img: "../assets/img//insumos/22.png",
+    img: "./assets/img//insumos/22.png",
   },
   {
     nombre: "Azucar refinada",
-    img: "../assets/img//insumos/23.jpg",
+    img: "./assets/img//insumos/23.jpg",
   },
   {
     nombre: "Canela en polvo",
-    img: "../assets/img//insumos/25.jpg",
+    img: "./assets/img//insumos/25.jpg",
   },
   {
     nombre: "Romero",
-    img: "../assets/img//insumos/26.png",
+    img: "./assets/img//insumos/26.png",
   },
   {
     nombre: "Hierbabuena",
-    img: "../assets/img//insumos/27.png",
+    img: "./assets/img//insumos/27.png",
   },
   {
     nombre: "Naranja",
-    img: "../assets/img//insumos/31.png",
+    img: "./assets/img//insumos/31.png",
   },
   {
     nombre: "Frutos rojos",
-    img: "../assets/img//insumos/29.jpg",
+    img: "./assets/img//insumos/29.jpg",
   },
   {
     nombre: "Limón Verde",
-    img: "../assets/img//insumos/30.jpg",
+    img: "./assets/img//insumos/30.jpg",
   },
   {
     nombre: "Limón amarillo",
-    img: "../assets/img//insumos/31.png",
+    img: "./assets/img//insumos/31.png",
   },
   {
     nombre: "Toronja",
-    img: "../assets/img//insumos/32.jpg",
+    img: "./assets/img//insumos/32.jpg",
   },
   {
     nombre: "Pepino",
-    img: "../assets/img//insumos/33.jpg",
+    img: "./assets/img//insumos/33.jpg",
   },
   {
     nombre: "Popotes",
-    img: "../assets/img//insumos/35.jpg",
+    img: "./assets/img//insumos/35.jpg",
   },
   {
     nombre: "Servilletas",
-    img: "../assets/img//insumos/37.jpg",
+    img: "./assets/img//insumos/37.jpg",
   },
 ];
